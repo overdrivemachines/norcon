85e371f0b0bbbe0dab10188525d74983c8be0723
Dipen Chauhan <get.dipen@gmail.com>
2019-01-25 11:58:59 -0800

    Added welcome controller

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,19 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def speakers
+  end
+
+  def location
+  end
+
+  def sponsors
+  end
+
+  def about
+  end
+
+  def party
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  get 'welcome/index'
+  get 'welcome/speakers'
+  get 'welcome/location'
+  get 'welcome/sponsors'
+  get 'welcome/about'
+  get 'welcome/party'
+  get '/2018', to: redirect('https://williamcaput.wixsite.com/norcon')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
