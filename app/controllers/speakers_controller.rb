9df246851305b9bcffc2bcd4c317ff3da4b0a3f7
Dipen Chauhan <get.dipen@gmail.com>
2019-02-08 11:47:13 -0800

    Added speakers controller and views

---
+++ app/controllers/speakers_controller.rb
@@ -0,0 +1,10 @@
+class SpeakersController < ApplicationController
+  def index
+  end
+
+  def speakers_2018
+  end
+
+  def speakers_2017
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  get 'welcome/index'
-  get 'welcome/speakers'
-  get 'welcome/location'
-  get 'welcome/sponsors'
-  get 'welcome/about'
-  get 'welcome/party'
+  get 'index', to: 'welcome#index'
+  # TODO
+  # get 'welcome/party'
+
+  get 'speakers/index'
+  get 'speakers/2018', to: 'speakers#speakers_2018'
+  get 'speakers/2017', to: 'speakers#speakers_2017'
+
   get '/2018', to: redirect('https://williamcaput.wixsite.com/norcon')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
