6e2dac12044a18d4726c00842ec2b28c31d9f84f
Dipen Chauhan <get.dipen@gmail.com>
2019-02-13 03:03:42 -0800

    Added schedule route

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get 'index', to: 'welcome#index'
-  # get 'schedule', to: 'welcome#schedule'
+  get 'schedule', to: 'welcome#schedule'
   # TODO
   # get 'welcome/party'
 
